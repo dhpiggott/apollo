f62e99b5943619e31e431a3b00f014d3643ef554
David Piggott <david@piggott.me.uk>
2020-10-13 10:42:46 +0100

    Update to ZIO 1.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,6 +7,6 @@ object Dependencies {
   val organizeImports: ModuleID =
     "com.github.liancheng" %% "organize-imports" % "0.4.0"
 
-  val zioCore: ModuleID = "dev.zio" %% "zio" % "1.0.0"
+  val zioCore: ModuleID = "dev.zio" %% "zio" % "1.0.3"
 
 }
