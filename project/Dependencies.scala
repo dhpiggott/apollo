f6a467e0822075c9acfc07be04ed0b3403a4c1cf
David Piggott <david@piggott.me.uk>
2020-10-13 10:42:00 +0100

    Update to Fastparse 2.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val fastParse: ModuleID = "com.lihaoyi" %% "fastparse" % "2.2.2"
+  val fastParse: ModuleID = "com.lihaoyi" %% "fastparse" % "2.3.0"
 
   val organizeImports: ModuleID =
     "com.github.liancheng" %% "organize-imports" % "0.4.0"
